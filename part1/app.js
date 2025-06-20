55ad10496ff5c0a39d6649f445ece52c3801dd51
a1860789 <a1860789@adelaide.edu.au>
2025-06-20 03:31:18 +0000

    [automatic commit] 2025-06-20 03:31:18 UTC 138.217.192.198

--- part1/app.js
+++ part1/app.js
@@ -112,6 +112,4 @@ app.get('/api/walkers/summary', async (req, res) => {
   }
 });
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(express.static)
