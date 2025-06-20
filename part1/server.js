bd260019e1a39f1033fb90f3ce47bd59adf06205
a1860789 <a1860789@adelaide.edu.au>
2025-06-20 03:35:10 +0000

    [automatic commit] 2025-06-20 03:35:10 UTC 138.217.192.198

--- part1/server.js
+++ part1/server.js
@@ -6,6 +6,6 @@ const http = require('http');
 const PORT = process.env.PORT || 8080;
 const server = http.createServer(app)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
